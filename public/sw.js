f2b0bdaa46dd07baadcc5b9ee0410b8f7ee6bfdd
actiongamyoun <actiongamyoun@gmail.com>
2026-05-24 21:21:38 +0900

    Add files via upload

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
 // 비밀고백 PWA Service Worker - v5 (network-first)
-const CACHE_VERSION = 'bimilgobaek-v5.3';
+const CACHE_VERSION = 'bimilgobaek-v5.4';
 const STATIC_ASSETS = [
   '/',
   '/index.html',
